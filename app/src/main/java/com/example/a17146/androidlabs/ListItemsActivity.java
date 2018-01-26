ba42e89e7035edb6bdd14fc758c389626c1cd5bf
17146 <kx19970925@gmail.com>
2018-01-26 16:13:29 -0500

    Added 3 activities for Lab2

--- app/src/main/java/com/example/a17146/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/a17146/androidlabs/ListItemsActivity.java
@@ -3,15 +3,11 @@ package com.example.a17146.androidlabs;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class LoginActivity extends Activity {
+public class ListItemsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-
-
+        setContentView(R.layout.activity_list_items);
     }
 }
